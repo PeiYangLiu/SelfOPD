65c544ae3eda5006c33d74d36b674fcda9515eb8
PeiyangLiu <478220635@qq.com>
2026-03-15 15:24:59 +0000

    add eval long

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -733,6 +733,8 @@ class RayPPOTrainer:
                 "validate": True,
                 "global_steps": self.global_steps,
             }
+            if self.config.actor_rollout_ref.rollout.val_kwargs.get('val_max_response_length'):
+                test_gen_batch.meta_info["val_max_response_length"] = self.config.actor_rollout_ref.rollout.val_kwargs.val_max_response_length
             print(f"test_gen_batch meta info: {test_gen_batch.meta_info}")
 
             # pad to be divisible by dp_size
